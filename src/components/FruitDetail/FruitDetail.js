9f8c063a1feec98d74a955145d3365fe4db5ad0e
eduardo rezende cruz <edu_rezende11@hotmail.com>
2021-05-05 22:58:18 -0300

    hotfix

--- src/components/FruitDetail/FruitDetail.js
+++ src/components/FruitDetail/FruitDetail.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../services';
-import instanceAxios from '../../services/axios';
 import './FruitDetail.css';
 
 const FruitDetail = ({ match }) => {
